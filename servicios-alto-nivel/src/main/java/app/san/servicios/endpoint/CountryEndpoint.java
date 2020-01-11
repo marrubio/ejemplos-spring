36232b0d81640973d4738eb86b8b6e6f8e38a0e3
marrubio <marugi@gmail.com>
2020-01-11 13:35:31 +0100

    Version inicial

---
+++ servicios-alto-nivel/src/main/java/app/san/servicios/endpoint/CountryEndpoint.java
@@ -0,0 +1,32 @@
+package app.san.servicios.endpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import app.san.servicios.ws.test.GetCountryRequest;
+import app.san.servicios.ws.test.GetCountryResponse;
+import app.san.servicios.repository.CountryRepository;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://san.app/servicios/ws/test";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
---
+++ servicios-alto-nivel/src/test/java/app/san/servicios/ServiciosNpaApplicationTests.java
@@ -0,0 +1,13 @@
+package app.san.servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosNpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
