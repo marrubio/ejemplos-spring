dd83e9ffdc5a70758a5fbb388ec7c137d936de78
marrubio <marugi@gmail.com>
2020-01-15 21:40:00 +0100

    prueba definicio den servicios

--- servicios-alto-nivel/src/main/java/app/san/servicios/endpoint/MyEndpoint.java
+++ servicios-alto-nivel/src/main/java/app/san/servicios/endpoint/MyEndpoint.java
@@ -1,24 +1,40 @@
 package app.san.servicios.endpoint;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import app.san.servicios.ws.usuarios.ObtenerPrincipal;
+import app.san.servicios.ws.usuarios.GetUserResponse;
+import app.san.servicios.ws.usuarios.TipoUsuario;
+import app.san.servicios.ws.usuarios.Usuario;
+
 @Endpoint
 public class MyEndpoint {
-	private static final String NAMESPACE_URI = "http://san.app/servicios/ws/my";
-	
+	private static final String NAMESPACE_URI = "http://san.app/servicios/ws/usuarios";
+    private static final Logger LOGGER=LoggerFactory.getLogger(MyEndpoint.class);
 
 	@Autowired
 	public MyEndpoint() {		
 	}
 
-	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getMy")
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "obtenerPrincipal")
 	@ResponsePayload
-	public String getMy(@RequestPayload String request) {
-		String response = "Hola " + request + " !!";		
+	public GetUserResponse getMy(@RequestPayload ObtenerPrincipal request) {
+        LOGGER.info("getMy()");
+        
+        GetUserResponse response = new GetUserResponse();
+		
+		Usuario dev = new Usuario();
+		dev.setNombre("developer");
+		dev.setLogin("dev");
+		dev.setPassword("dev123");
+		dev.setTipo(TipoUsuario.DEV);
+		response.setUser(dev);		
 		return response;
 	}
 }
--- servicios-alto-nivel/src/main/java/app/san/servicios/endpoint/UserEndpoint.java
+++ servicios-alto-nivel/src/main/java/app/san/servicios/endpoint/UserEndpoint.java
@@ -6,13 +6,19 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+
 import app.san.servicios.ws.usuarios.GetUserRequest;
 import app.san.servicios.ws.usuarios.GetUserResponse;
 import app.san.servicios.repository.UsersRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Endpoint
 public class UserEndpoint {
+	
 	private static final String NAMESPACE_URI = "http://san.app/servicios/ws/usuarios";
+    private static final Logger LOGGER=LoggerFactory.getLogger(UserEndpoint.class);
 
 	private UsersRepository usersRepository;
 
@@ -23,7 +29,9 @@ public class UserEndpoint {
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getUserRequest")
 	@ResponsePayload
-	public GetUserResponse getCountry(@RequestPayload GetUserRequest request) {
+	public GetUserResponse getUserRequest(@RequestPayload GetUserRequest request) {
+        LOGGER.info("getUserRequest()");
+        
 		GetUserResponse response = new GetUserResponse();
 		response.setUser(usersRepository.findUsuario(request.getName()));
 
